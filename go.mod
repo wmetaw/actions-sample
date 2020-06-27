77217ee70ebfdb00e38b3341de32884dcda8735a
wMETAw <wgn8404@gmail.com>
2020-06-27 15:20:52 +0900

    hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wmetaw/actions-sample
+
+go 1.14
---
+++ main_test.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSayHello(t *testing.T) {
+	fmt.Println(SayHello())
+}
